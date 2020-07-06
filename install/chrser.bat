9bb13ff949d3cc95f082d30e21c8e6d75059def0
Hansrenee Prayitno <hansrenee@live.com>
2020-07-06 16:13:14 +0700

    added installation script

---
+++ install/chrser.bat
@@ -0,0 +1,11 @@
+REM Installer Chronome CapsLock general Driver 2020
+REM ChronomeDev 2020
+REM ========== HARAP TUNGGU ========================
+%cd%
+@echo off
+PowerShell -Command "Add-Type -AssemblyName PresentationFramework;[System.Windows.MessageBox]::Show('Selamat Datang pada instalasi Shell ini', 'ChronomeDriver')"
+cls
+xcopy %~dp0chrservice.exe "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp"
+cls
+PowerShell -Command "Add-Type -AssemblyName PresentationFramework;[System.Windows.MessageBox]::Show('Pastikan setelah halaman shell close, restart komputer. Apabila driver tidak berfungsi, coba kembali dengan menjalankan instalasi ini dengan hak Administrator', 'ChronomeDriver')"
+exit
